bb1e1d60f54e92287a646034425eacac103075db
Gavle-hub <erikdaniel650@gmail.com>
2021-12-14 23:01:22 -0800

    a

---
+++ index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import  ReactDOM from "react-dom";
+
+const hello = () =>  {
+    return <div>hsiuoefhosi</div>
+}
+ ReactDOM.render(<hello />, document.getElementById("root"));
